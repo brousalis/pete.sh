40ab46d8b6684490441b90e3c3063912b1e9ed84
Pete Brousalis <brousapg@gmail.com>
2022-12-07 17:50:55 -0600

    add basic API to get and save links

---
+++ pages/api/shorten.tsx
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let data;
+
+  try {
+    data = await prisma.link.create({
+      data: {
+        slug: req.query.slug,
+        url: req.query.url,
+      },
+    });
+  } catch (error) {
+    console.error('POST error', error);
+    res.status(500).json({ error: 'Error creating link' });
+  }
+
+  if (!data) {
+    res.status(500).json({ error: 'Unexpected error' });
+    return;
+  }
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.json(data);
+
+  return;
+};
---
+++ pages/api/slug.tsx
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import prisma from '../../lib/prisma';
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query.slug;
+
+  if (!slug) {
+    res.status(404).json({ error: 'Invalid request' });
+    return;
+  }
+
+  const data = await prisma.link.findFirst({
+    where: {
+      slug: { equals: slug },
+    },
+  });
+
+  if (!data) {
+    res.status(404).json({ error: 'Invalid link' });
+    return;
+  }
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  res.json(data);
+
+  return;
+};
