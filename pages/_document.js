8e194fcefaddec473137f3949f435f2c4309c84a
Pete Brousalis <brousapg@gmail.com>
2022-12-07 16:37:04 -0600

    little tweaking to the form

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html className="h-full bg-gray-200" lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        </Head>
+        <body className="h-full transition-colors duration-100 ease-linear">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
-import Head from 'next/head';
+import Head from 'next/head'
 
-import CreateLink from '../components/CreateLink';
+import CreateLink from '../components/CreateLink'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
       <Head>
         <title>pete.sh(ort)</title>
-        <meta name='description' content='pete brousalis, brousalis' />
+        <meta name="description" content="a really good url shortener" />
       </Head>
+
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-semibold tracking-tight text-gray-900">
+          <span className="text-gray-900">pete.sh</span>
+          <span className="text-gray-500">ort</span>
+        </h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          a really good url shortener
+        </p>
+      </div>
 
       <CreateLink />
     </div>
-  );
+  )
 }
