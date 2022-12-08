076f8a71447f8b9108bec7dc9f619a6f45295386
Pete Brousalis <brousapg@gmail.com>
2022-12-07 23:17:06 -0600

    add a quick loading state

--- app/components/Alert.tsx
+++ app/components/Alert.tsx
@@ -3,9 +3,13 @@ import { CheckCircleIcon } from '@heroicons/react/20/solid';
 import React from 'react';
 
 function Alert({
+  children,
   shortenedUrl,
+  title,
   onClose,
 }: {
+  children?: React.ReactNode;
+  title?: string;
   shortenedUrl?: string;
   onClose: () => void;
 }) {
@@ -19,21 +23,17 @@ function Alert({
           />
         </div>
         <div className="ml-3 flex flex-1 flex-col">
-          <h3 className="text-sm font-medium text-green-800">Link shortened</h3>
-          <div className="mt-2 text-sm text-green-700">
-            <a href={shortenedUrl} rel="noreferrer" target="_blank">
-              {shortenedUrl}
-            </a>
-          </div>
+          <h3 className="text-sm font-medium text-green-800">{title}</h3>
+          <div className="mt-2 text-sm text-green-700">{children}</div>
           <div className="ml-auto mt-4">
             <div className="-mx-2 -my-1.5 flex">
               <CopyButton text={shortenedUrl} />
               <button
                 type="button"
                 onClick={onClose}
-                className="ml-3 rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
+                className="ml-3 rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 active:bg-green-100"
               >
-                Done
+                done
               </button>
             </div>
           </div>
